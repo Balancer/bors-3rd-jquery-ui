da37b8d0389998ecd23bbe7b896ca56f29292275
Balancer <balancer@balancer.ru>
2017-02-12 05:12:30 +0300

    https fixes

--- classes/jquery/ui.php
+++ classes/jquery/ui.php
@@ -5,7 +5,7 @@ class jquery_ui extends jquery_plugin
 	static function load()
 	{
 //		parent::load('jquery-ui', 'jquery.jquery-ui', 'balancer/bors-3rd-jquery-ui');
-		jquery::plugin('http://code.jquery.com/ui/1.8.3/jquery-ui.min.js');
-		bors_use('http://code.jquery.com/ui/1.8.3/themes/base/jquery-ui.css');
+		jquery::plugin('//code.jquery.com/ui/1.8.3/jquery-ui.min.js');
+		bors_use('//code.jquery.com/ui/1.8.3/themes/base/jquery-ui.css');
 	}
 }
